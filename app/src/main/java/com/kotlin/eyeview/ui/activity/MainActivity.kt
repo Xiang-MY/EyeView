14cef4478cbb313c982fedda3196b6f395d82d49
wykeke <email@2565556263@qq.com>
2022-04-13 16:07:09 +0800

    video

--- app/src/main/java/com/kotlin/eyeview/ui/activity/MainActivity.kt
+++ app/src/main/java/com/kotlin/eyeview/ui/activity/MainActivity.kt
@@ -2,24 +2,23 @@ package com.kotlin.eyeview.ui.activity
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.BitmapFactory
-import android.net.Uri
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.core.content.FileProvider
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.kotlin.eyeview.R
 import com.kotlin.eyeview.ui.fragment.Adjust_Fragment
+import com.kotlin.eyeview.ui.fragment.AppUsage_Fragment
 import com.kotlin.eyeview.ui.fragment.Eye_Fragment
 import com.kotlin.eyeview.ui.fragment.We_Fragment
+import com.kotlin.eyeview.ui.useapptime.AppUsageActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
+import com.kotlin.eyeview.ui.useapptime.UseActivity
+
+//import useapptime.Use_Fragment
 
 class MainActivity : AppCompatActivity() ,View.OnClickListener{
 
@@ -27,67 +26,58 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
     var mEye_Fragment = Eye_Fragment()
     var mAdjust_Fragment = Adjust_Fragment()
     var mWe_Fragment = We_Fragment()
-//    var Blur_Fragment =
+//    var mUse_Fragment = Use_Fragment()
+    var app_Fragment = AppUsage_Fragment()
 
-    //调用相机
-    val takePhoto = 1
-    lateinit var imageUri: Uri
-    lateinit var outputImage: File
-    val TAG = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().add(R.id.fragment,mEye_Fragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.fragment, mEye_Fragment).commit()
 //        addFragment(1)
         //点击切换界面
         v1.setOnClickListener(this)
-//        v2.setOnClickListener(this)
+        v2.setOnClickListener(this)
         v3.setOnClickListener(this)
 
-        //点击调用相机
-        v2.setOnClickListener {
-            Log.d(TAG, "onCreate: 拍照")
-            //创建File对象，用于存储拍照后的图片
-            //externalCacheDir方法可以得到存储如片的目录
-            outputImage = File(externalCacheDir,"output_image.jpg")
-            if (outputImage.exists()){
-                outputImage.delete()
-            }
-            outputImage.createNewFile()
-            imageUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-                FileProvider.getUriForFile(this,"com.example.eyeview.fileprovider",outputImage)
-            } else {
-                Uri.fromFile(outputImage)
-            }
-            //启动相机程序
-            val intent = Intent("android.media.action.IMAGE_CAPTURE")
-            Log.d("testt","调用相机")
-            intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri)
-            startActivityForResult(intent,takePhoto)
+//        v2.setOnClickListener{
+////            getWindowHeightOrWidth(this)
+////            Log.d("屏幕宽高", "onDraw: $screenWidth $screenHeight")
+////            screenList.add(screenWidth)
+////            screenList.add(screenHeight)
+//            val intent = Intent(this, AppUsageActivity::class.java)
+////            intent.putIntegerArrayListExtra("screenList",screenList)
 //            startActivity(intent)
-        }
+//        }
+
 
     }
 
     override fun onClick(view: View?) {
         val fragmentManager = supportFragmentManager //获取FragmentManager实例
         val transaction = fragmentManager.beginTransaction() //开启一个事物
+//        hideFragments(transaction)
+//        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+//        transaction.setCustomAnimations(R.anim.slide_left_in,R.anim.slide_right_out,R.anim.slide_right_in,R.anim.slide_left_out)
+//        transaction.setCustomAnimations(R.anim.slide_right_in,R.anim.slide_left_out,R.anim.slide_left_in,R.anim.slide_right_out)
         var f : Fragment? = null
         when(view?.id) {
             R.id.v1 -> {
                 //如果不是主界面则切换到主界面，
                 f = Eye_Fragment()
             }
+            R.id.v2 -> f = AppUsage_Fragment()
 //            R.id.v2 -> f = Adjust_Fragment()
+//            R.id.v2 -> f = Use_Fragment()
             R.id.v3 -> f = We_Fragment()
         }
 
         //切换
         if (f != null) {
-            transaction.replace(R.id.fragment,f)
+            Log.d("测试点击", "onClick: "+f)
+            transaction.replace(R.id.fragment, f)
         }
         transaction.commit() //提交事务
 
@@ -98,63 +88,72 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
 //        }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        when(requestCode){
+//            takePhoto -> {
+//                if (resultCode == Activity.RESULT_OK){
+//                    val intent = Intent(this, CameraActivity::class.java)
+//                    //直接传bitmap过大会报错，所以在activity之间传送uri最后得到图片
+//                    intent.data = imageUri
+//                    startActivity(intent)
+//                }
+//            }
+//        }
+//    }
 
-        when(requestCode){
-            takePhoto -> {
-                if (resultCode == Activity.RESULT_OK){
-                    val intent = Intent(this, CameraActivity::class.java)
-                    //直接传bitmap过大会报错，所以在activity之间传送uri最后得到图片
-                    intent.data = imageUri
-                    startActivity(intent)
-                }
-            }
-        }
-    }
-
-    private fun addFragment(i: Int) {
-        //开启事务，fragment的控制是由事务来实现的
-        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        hideFragments(transaction)
-        when (i) {
-            1 -> {
-                // Toast.makeText(this,"加载页面",Toast.LENGTH_SHORT).show()
-                if (mEye_Fragment == null) {
-                    mEye_Fragment = Eye_Fragment()
-                    transaction.add(R.id.fragment, mEye_Fragment).commit()
-                    Toast.makeText(this,"加载页面", Toast.LENGTH_SHORT).show()
-                } else {
-                    transaction.show(mEye_Fragment)
-                    Toast.makeText(this,"加载", Toast.LENGTH_SHORT).show()
-                }
-            }
-            2 -> {
-
-                if (mAdjust_Fragment == null) {
-                    mAdjust_Fragment = Adjust_Fragment()
-                    transaction.add(R.id.fragment, mAdjust_Fragment).commit()
-                } else {
-                    transaction.show(mAdjust_Fragment)
-                }
-            }
-            3 -> {
-
-                if (mWe_Fragment == null) {
-                    mWe_Fragment = We_Fragment()
-                    transaction.add(R.id.fragment, mWe_Fragment).commit()
-                } else {
-                    transaction.show(mWe_Fragment)
-                }
-            }
-        }
-        transaction.commitAllowingStateLoss()
-    }
+//    private fun addFragment(i: Int) {
+//        //开启事务，fragment的控制是由事务来实现的
+//        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+//        hideFragments(transaction)
+//        when (i) {
+//            1 -> {
+//                // Toast.makeText(this,"加载页面",Toast.LENGTH_SHORT).show()
+//                if (mEye_Fragment == null) {
+//                    mEye_Fragment = Eye_Fragment()
+//                    transaction.add(R.id.fragment, mEye_Fragment).commit()
+//                    Toast.makeText(this, "加载页面", Toast.LENGTH_SHORT).show()
+//                } else {
+//                    transaction.show(mEye_Fragment)
+//                    Toast.makeText(this, "加载", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//            2 -> {
+////                if (mUse_Fragment == null) {
+////                    mUse_Fragment = Use_Fragment()
+////                    transaction.add(R.id.fragment, mUse_Fragment).commit()
+////                } else {
+////                    transaction.show(mUse_Fragment)
+////                }
+//
+//                if (mAdjust_Fragment == null) {
+//                    mAdjust_Fragment = Adjust_Fragment()
+//                    transaction.add(R.id.fragment, mAdjust_Fragment).commit()
+//                } else {
+//                    transaction.show(mAdjust_Fragment)
+//                }
+//            }
+//            3 -> {
+//
+//                if (mWe_Fragment == null) {
+//                    mWe_Fragment = We_Fragment()
+//                    transaction.add(R.id.fragment, mWe_Fragment).commit()
+//                } else {
+//                    transaction.show(mWe_Fragment)
+//                }
+//            }
+//        }
+//        transaction.commitAllowingStateLoss()
+//    }
 
 
     private fun hideFragments(transaction: FragmentTransaction) {
-        if (mAdjust_Fragment != null) {
-            transaction.hide(mAdjust_Fragment)
+//        if (mUse_Fragment != null) {
+//            transaction.hide(mUse_Fragment)
+//        }
+        if (app_Fragment != null) {
+            transaction.hide(app_Fragment)
         }
         if (mEye_Fragment != null) {
             transaction.hide(mEye_Fragment)
--- app/src/main/java/com/kotlin/eyeview/ui/activity/VideoActivity.kt
+++ app/src/main/java/com/kotlin/eyeview/ui/activity/VideoActivity.kt
@@ -1,13 +1,24 @@
 package com.kotlin.eyeview.ui.activity
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
 import android.net.Uri.parse
+import android.os.Build
 import android.os.Bundle
+import android.renderscript.Allocation
+import android.renderscript.Element
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicBlur
+import android.util.Log
 import android.widget.MediaController
 import android.widget.RelativeLayout
+import android.widget.Toast
 import android.widget.VideoView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.kotlin.eyeview.R
+import kotlinx.android.synthetic.main.activity_video.*
 
 
 class VideoActivity : AppCompatActivity() {
@@ -72,6 +83,60 @@ class VideoActivity : AppCompatActivity() {
 
         //播放
         videoView.start()
+//        Toast.makeText(applicationContext,"播放成功",Toast.LENGTH_SHORT).show()
+//        button.setOnClickListener {
+//            screenshot()
+//        }
+        val c = 10;
+        if (c == 10){
+            screenshot()
+        }
 
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private fun screenshot() {
+        // 获取屏幕
+        val dView = window.decorView
+        dView.isDrawingCacheEnabled = true
+        dView.buildDrawingCache()
+        val bmp = dView.drawingCache
+
+        if (bmp != null) {
+            try {
+//                Log.d("成功", "screenshot: ")
+                Toast.makeText(applicationContext,"截图成功",Toast.LENGTH_LONG).show()
+                val blurmap = blurBitmap(baseContext, bmp)
+
+//                screenImage.setImageBitmap(blurmap)
+            } catch (e: Exception) {
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    fun blurBitmap(context: Context?, bitmap: Bitmap): Bitmap? {
+        //用需要创建高斯模糊bitmap创建一个空的bitmap
+        val outBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        // 初始化Renderscript，该类提供了RenderScript context，创建其他RS类之前必须先创建这个类，其控制RenderScript的初始化，资源管理及释放
+        val rs = RenderScript.create(context)
+        // 创建高斯模糊对象
+        val blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
+        // 创建Allocations，此类是将数据传递给RenderScript内核的主要方 法，并制定一个后备类型存储给定类型
+        val allIn = Allocation.createFromBitmap(rs, bitmap)
+        val allOut = Allocation.createFromBitmap(rs, outBitmap)
+        //设定模糊度(注：Radius最大只能设置25.f)
+        blurScript.setRadius(10f)
+        // Perform the Renderscript
+        blurScript.setInput(allIn)
+        blurScript.forEach(allOut)
+        // Copy the final bitmap created by the out Allocation to the outBitmap
+        allOut.copyTo(outBitmap)
+        // recycle the original bitmap
+        // bitmap.recycle();
+        // After finishing everything, we destroy the Renderscript.
+        rs.destroy()
+        return outBitmap
     }
 }
